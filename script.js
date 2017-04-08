db7f8f87ab221a33fa6ff9da76d684a9d5fd7993
James <james.d.mcgarr@students.ittralee.ie>
2017-04-08 03:04:51 +0000

    imgsrc and minify tips

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+/*global angular*/
 
+var app = angular.module("app", []);
 
-var app = angular.module("app", [])
-    .controller("myCtrl", function ($scope) {
-        $scope.test = "hello test";
-    });
+var myCtrl = function ($scope, $http) {
+    $scope.test = "hello test";
+    
+    $http.get("https://api.github.com/users/jamesdaniel").
+        then(function (response) {
+            $scope.imgUrl = response.data.avatar_url;
+            
+        }, function (err) {
+            console.log(JSON.stringify(err, null, 2));
+        })
+};
+
+app.controller("myCtrl", ["$scope", "$http", myCtrl]);
