22d9e6ead49957065ec9f2ddf8a39806e610093b
James <james.d.mcgarr@students.ittralee.ie>
2017-04-08 03:45:27 +0000

    ng-submit ng-click .function()

--- script.js
+++ script.js
@@ -8,10 +8,27 @@ var myCtrl = function ($scope, $http) {
     $http.get("https://api.github.com/users/jamesdaniel").
         then(function (response) {
             $scope.imgUrl = response.data.avatar_url;
-            
         }, function (err) {
             console.log(JSON.stringify(err, null, 2));
-        })
+        });
+    
+    $scope.increment = function (number) {
+        $scope.number = parseInt(number) + 1;
+    };
 };
 
+var myCtrl2 = function ($scope, $http) {
+    $scope.testing = "testing here mister";
+    console.log($scope.testing);
+    
+    
+    $http.get("https://api.github.com/users/angular/repos")
+    .then(function (res) {
+        $scope.repos = res.data;
+    }, function (res) {
+        console.log(JSON.stringify(res.data, null, 2));
+    })
+}
+
 app.controller("myCtrl", ["$scope", "$http", myCtrl]);
+app.controller("myCtrl2", ["$scope", "$http", myCtrl2]);
